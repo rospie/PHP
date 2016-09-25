3402e9e7aa403140ea5c070f4f370a51b2ca71cc
Rosie <marta.deskiewicz@gmail.com>
2016-09-25 16:26:39 +0200

    small corrections

--- menu.php
+++ menu.php
@@ -1,9 +1,10 @@
 <link rel="stylesheet" type="text/css" href="active_menu.css">
 
-
-<ul>
-	<li><a href="menu_p1.php"<?php if($curpage == 'menu_p1.php') {echo 'class="active"';}?>>page1</a></li>
-    <li><a href="menu_p2.php"<?php if($curpage == 'menu_p2.php') {echo 'class="active"';}?>>page2</a></li>
-    <li><a href="menu_p3.php"<?php if($curpage == 'menu_p3.php') {echo 'class="active"';}?>>page3</a></li>
-    <li><a href="menu_p4.php"<?php if($curpage == 'menu_p4.php') {echo 'class="active"';}?>>page4</a></li>
-</ul>
+<nav>
+	<ul>
+		<li><a href="menu_p1.php"<?php if($curpage == 'menu_p1.php') {echo 'class="active"';}?>>home</a></li>
+    	<li><a href="menu_p2.php"<?php if($curpage == 'menu_p2.php') {echo 'class="active"';}?>>about</a></li>
+    	<li><a href="menu_p3.php"<?php if($curpage == 'menu_p3.php') {echo 'class="active"';}?>>project</a></li>
+    	<li><a href="menu_p4.php"<?php if($curpage == 'menu_p4.php') {echo 'class="active"';}?>>contact</a></li>
+	</ul>
+</nav>
